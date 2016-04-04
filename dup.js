ca02a3b3b8ae398d8d530a8cdd6de6902646c796
ken <block24block@gmail.com>
2016-04-04 18:24:51 +0800

    refactor for future enhance

--- dup.js
+++ dup.js
@@ -1,56 +1,14 @@
-document.addEventListener('DOMContentLoaded', function(){
-  chrome.bookmarks.getTree(function(node){
-    var hash = Object.create(null);
-    
-    function add(node){
-      hash[node.title] || (hash[node.title] = []);
-      hash[node.title].push(node);
-    }
-    
-    var bookmarks = node[0].getBookmarkDescendants();
-    
-    bookmarks.forEach(function(node){
-      add(node);
-    });
-    
-    var dup = [];
-    
-    var dup_count = 0;
-    var getAncestors_done_count = 0;
+function renderBM(bookmarks){
 
-    Object.keys(hash).forEach(function(k){
-      var arr = hash[k];
-      if(arr.length > 1){
-        dup_count = dup_count + arr.length;
-        arr.forEach(function(node){
-          node.getAncestors(function(ans){
-            node.ancestors = ans;
-            getAncestors_done_count = getAncestors_done_count + 1;
-          });
-        });
-        var o = Object.create(null);
-        o['title'] = k;
-        o['bookmarks'] = arr;
-        dup.push(o);
-      }
+    var groupByTitle = _.groupBy(bookmarks, function(bm){return bm.title;});
+
+    var dup = _.reduce(groupByTitle, function(dup, grp, title){
+                  if(grp.length > 1) dup[title] = grp;
+                  return dup;
+                  }, Object.create(null));
+    
+    $.get("tmpl/dup.tmpl").then(function(tm){
+      $("#main").html(_.template(tm)({dup: dup}));
     });
 
-    function format(obj){
-      var list = '<ul>' + obj.bookmarks.map(function(bm){return '<li>' + bm.ancestors.reverse().map(function(dir){return dir.title;}).join("/") + '</li>'; }).join("") + '</ul>';
-      return '<h3>' + obj.title + '</h3>' + list;
-    }
-    
-    (function print(){
-      if(getAncestors_done_count === dup_count){
-        var html = dup.map(function(obj){
-          return format(obj);
-        }).join("");
-        document.getElementById("main").innerHTML = html;
-      }else{
-        setTimeout(arguments.callee, 0);
-      }
-    })();
-
-
-  });
-});
+}
--- extend.js
+++ extend.js
@@ -1,24 +1,22 @@
-var node_proto = Object.prototype;
-
-node_proto.getAncestors = function(callback){
+function getAncestors(node, callback){
   var ps = [];
   function get_parent_node(pid){
-    pid ? chrome.bookmarks.get(pid, after_get_node) : callback(ps);
+    pid ? chrome.bookmarks.get(pid, after_get_node) : callback(ps, node);
   }
   function after_get_node(nodes){
     p_node = nodes[0];
     ps.push(p_node);
     get_parent_node(p_node.parentId);
   }
-  get_parent_node(this.parentId);
+  get_parent_node(node.parentId);
 };
 
-node_proto.getDescendants = function(filter){
+function getDescendants(node, filter){
   var desc = [];
-  if(this.children){
-    this.children.forEach(function(c){
+  if(node.children){
+    node.children.forEach(function(c){
       filter ? (filter(c) && desc.push(c)) : desc.push(c) ;
-      c.getDescendants(filter).forEach(function(d){
+      getDescendants(c, filter).forEach(function(d){
         desc.push(d);
       });
     });
@@ -26,8 +24,8 @@ node_proto.getDescendants = function(filter){
   return desc;
 };
 
-node_proto.getBookmarkDescendants = function(){
-  return this.getDescendants(function(node){
+function getBookmarkDescendants(node){
+  return getDescendants(node, function(node){
     return node['children'] === undefined;
   });
 };
