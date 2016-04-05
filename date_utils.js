22b30820c9a96463587ce2f46e1ae1c62f93d35f
ken <block24block@gmail.com>
2016-04-05 23:17:50 +0800

    use keys to find templates

---
+++ date_utils.js
@@ -0,0 +1,33 @@
+(function(win, u){
+
+  win.dut = u;
+
+  function padZero(number) {  
+    return (number < 10 ? "0" + number : number);
+  }
+
+  function chromeDateFormater(chromeDate){
+    var dateObject = new Date(chromeDate),
+        temp = [];
+
+    for(var i = 1; i < arguments.length; i++){
+      var ele = arguments[i];
+      temp.push((typeof ele === "function" ? ele(dateObject) : ele));
+    }
+
+    return temp.join("");
+  }
+
+  u.yy = function(date){
+    return date.getUTCFullYear().toString().slice(2);
+  };
+
+  u.mm = function(date){
+    return padZero(date.getUTCMonth() + 1);
+  };
+
+  u.cd2yymm = function(number){
+    return chromeDateFormater(number, u.yy, u.mm);
+  };
+
+})(window, Object.create(null));
--- render.js
+++ render.js
@@ -2,15 +2,42 @@ function renderBM(root){
 
     var bookmarks = getBookmarkDescendants(root);
 
-    var groupByTitle = _.groupBy(bookmarks, function(bm){return bm.title;});
+    var dirs = getDirDescendants(root);
 
-    var dup = _.reduce(groupByTitle, function(dup, grp, title){
-                  if(grp.length > 1) dup[title] = grp;
-                  return dup;
-                  }, Object.create(null));
+    var report = Object.create(null);
+
+    report.dup = _.reduce(
+                   _.groupBy(
+                     bookmarks,
+                     function(bm){return bm.title;}
+                   ),
+                   function(dup, grp, title){if(grp.length > 1) dup[title] = grp;return dup;},
+                   Object.create(null)
+                 );
+
+    report.dir_cap = _.sortBy(dirs, function(d){return d.children.length;}).reverse();
     
-    $.get("tmpl/dup.tmpl").then(function(tm){
-      $("#main").html(_.template(tm)({dup: dup}));
+    report.month = _.sortBy(
+                     _.map(
+                       _.groupBy(
+                         bookmarks,
+                         function(bm){return dut.cd2yymm(bm.dateAdded);}
+                       ),
+                       function(grp, m){return {month: m, bookmarks : grp}}
+                     ),
+                     function(obj){ return parseInt(obj.month);}
+                   );
+
+    var sub_report_names = _.keys(report);
+
+    $.get("tmpl/sub.tmpl").then(function(tm){
+      $("#main").html(_.template(tm)({subs: sub_report_names}));
+    });
+
+    _.each(sub_report_names, function(tmpl_name){
+      $.get("tmpl/" + tmpl_name + ".tmpl").then(function(tm){
+        $("#" + tmpl_name).html(_.template(tm)(report));
+      });
     });
 
 }
