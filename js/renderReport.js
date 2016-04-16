94ce52917449a98dbdda399aa8ef805565a3ab01
ken <block24block@gmail.com>
2016-04-16 20:33:06 +0800

    split renderLayout and renderReport

---
+++ js/renderReport.js
@@ -0,0 +1,45 @@
+define(["underscore"], function(_, rlayout){
+  return function(report){
+
+    _.each(report, function(r){
+      var print = print_action(r);
+      print();
+    });
+
+    function print_action(report){
+      return function(){
+
+        if(report.display === 'text') return;
+
+        var count = _.sortBy(
+          _.map(report.data, function(group, key){
+            return [key, group.length];
+          }),function(data){
+            return data[1];
+          }).reverse();
+
+        var org_data = [[report.name, 'count']].concat(count);
+
+        var data_table = google.visualization.arrayToDataTable(org_data);
+
+        var options = {
+          title: report.name,
+          pieHole: 0.4,
+          width: $(window).width() * 0.8,
+          height: $(window).height() * 0.8,
+        };
+
+        var chart = new google.visualization[report.display](document.getElementById(report.name.replace(/\s/g,'_')));
+        chart.draw(data_table, options);
+
+        google.visualization.events.addListener(chart, 'select', selectHandler);
+
+        function selectHandler(e) {
+          var selected = chart.getSelection()[0];
+          console.log(selected.row, report.data[org_data[selected.row + 1][0]]);
+        }
+      }
+    }
+
+  }
+});
